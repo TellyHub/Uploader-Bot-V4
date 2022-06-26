8b693a9c0843167d475c1677f907f739199ab9a1
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-26 13:57:48 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -185,7 +185,10 @@ async def echo(bot, update):
                     "video", format_id, format_ext, randem)
                 cb_string_file = "{}|{}|{}|{}".format(
                     "file", format_id, format_ext, randem)
-                if format_string is not None and not "audio only" in format_string:
+                if format_string.upper():
+                        continue
+                    format_ext = formats.get("ext")
+                    approx_file_size = ""
                     ikeyboard = [
                         InlineKeyboardButton(
                             "🎬 " + format_string + " (" + format_ext.upper() + ") " + approx_file_size + " ",
