a6d799421cffbb2606970aee83667834046366fa
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-29 10:37:58 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -100,7 +100,7 @@ async def echo(bot, update):
         command_to_exec = [
             "yt-dlp",
             "--no-warnings",
-            "--youtube-skip-hls-manifest",
+            "--allow-dynamic-mpd",
             "-j",
             url,
             "--proxy", Config.HTTP_PROXY
@@ -109,7 +109,7 @@ async def echo(bot, update):
         command_to_exec = [
             "yt-dlp",
             "--no-warnings",
-            "--youtube-skip-hls-manifest",
+            "--allow-dynamic-mpd",
             "-j",
             url
         ]
