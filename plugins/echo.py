02bf32a52c617d8f7063052fe7c41378dab31785
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-29 08:19:17 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -32,33 +32,52 @@ from pyrogram.types import Thumbnail
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
+        logger.info(update.from_user)
+        url = update.text
+        youtube_dl_username = None
+        youtube_dl_password = None
+        file_name = None
 
-            logger.info(update.from_user)
-            u = update.text
-            youtube_dl_username = None
-            youtube_dl_password = None
-            file_name = None
-            url = None
-            if "|" in u:
-               ul_part = u.strip(" ")
-               ul_parts = ul_part.split("|")
-               u = ul_parts[0]
- 
+        print(url)
+        if "|" in url:
+            url_parts = url.split("|")
+            if len(url_parts) == 2:
+                url = url_parts[0]
+                file_name = url_parts[1]
+            elif len(url_parts) == 4:
+                url = url_parts[0]
+                file_name = url_parts[1]
+                youtube_dl_username = url_parts[2]
+                youtube_dl_password = url_parts[3]
+            else:
+                for entity in update.entities:
+                     if entity.type == "text_link":
+                        url = entity.url
+                     elif entity.type == "url":
+                        o = entity.offset
+                        l = entity.length
+                        url = url[o:o + l]
+            if url is not None:
+                url = url.strip()
+            if file_name is not None:
+                file_name = file_name.strip()
+        # https://stackoverflow.com/a/761825/4723940
+            if youtube_dl_username is not None:
+                youtube_dl_username = youtube_dl_username.strip()
+            if youtube_dl_password is not None:
+                youtube_dl_password = youtube_dl_password.strip()
+            logger.info(url)
+            logger.info(file_name)
+        else:
+            for entity in update.entities:
+                if entity.type == "text_link":
+                url = entity.url
+                elif entity.type == "url":
+                    o = entity.offset
+                    l = entity.length
+                    url = url[o:o + l]
+                logger.info(update.from_user)
 
-            if "|" in update.text:
-                file_name = ul_parts[1]
-
-                if url is not None:
-                    url = url.strip()
-                if file_name is not None:
-                    file_name = file_name.strip()
-                # https://stackoverflow.com/a/761825/4723940
-                if youtube_dl_username is not None:
-                    youtube_dl_username = youtube_dl_username.strip()
-                if youtube_dl_password is not None:
-                    youtube_dl_password = youtube_dl_password.strip()
-                logger.info(url)
-                logger.info(file_name)
              
  
             if Config.HTTP_PROXY != "":
