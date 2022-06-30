8bb8ab203849b0999ffa2f666ceed8d42702bd6e
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-30 16:46:27 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -79,28 +79,7 @@ Expires on: {}"""
 ⏳ Tɪᴍᴇ ʟᴇғᴛ : {4}\n\n
 """
 
-class Text:
-    @staticmethod
-    async def join_channel_msg(m: Message):
-        join_channel = Config.UPDATE_CHANNEL.replace("@", "")
-        return await m.reply_text(
-            "You have reached your limit of usage."
-            "\nPlease wait for some time before using this bot again."
-            f"\nIf you want to increase the usage limit, join {Config.UPDATE_CHANNEL}",
-            reply_markup=ikb(
-                [[("Join Channel", f"https://t.me/{join_channel}", "url")]],
-            ),
-        )
 
-    @staticmethod
-    def ban_kb(user_id: int):
-        return (
-            ikb(
-                [[("Ban User", f"ban_{user_id}")]],
-            )
-            if user_id != Config.OWNER_ID
-            else None
-        )
 
 
 
