87a95c62a3dd3470f20f812371bff9dce9a8edf1
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-30 09:26:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,7 +55,9 @@ async def info_handler(bot, update):
   
     await update.reply_text(  
         text=Translation.INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status),             
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    
     )
 @Client.on_message(filters.private & filters.command("upgrade"))
 async def upgrade(bot, update):
@@ -65,5 +67,7 @@ async def upgrade(bot, update):
   
     await update.reply_text(  
         text=UPGRADE_TEXT,             
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    
     )
