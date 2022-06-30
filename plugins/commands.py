48ec7fa9283d4893dacd50f7f263427f8d9177a7
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-30 09:25:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,7 @@ async def start(bot, update):
 async def rate(bot, update):
             await bot.forward_messages(update.chat.id, "@Super_botz", Config.RATE_MSG_ID)
         
-@Client.on_message(filters.private & filters.command("info"))
+@Client.on_message(filters.private & filters.command("me"))
 async def info_handler(bot, update):
 
 
@@ -64,6 +64,6 @@ async def upgrade(bot, update):
 
   
     await update.reply_text(  
-        text=Translation.INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status),             
+        text=UPGRADE_TEXT,             
         disable_web_page_preview=True
     )
