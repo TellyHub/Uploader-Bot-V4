2663cac0003bdf8ebe8b50dbc52aafbff39078ab
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-06-30 16:45:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,25 @@ from plugins.functions.forcesub import handle_force_subscribe
 from pyrogram import enums, StopPropagation
 
 
-
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+    
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
 
 
 
