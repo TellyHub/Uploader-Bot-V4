f8026c91f379186e246650cfbff93d4ca934eb1a
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-03 12:31:51 +0530

    Update display_progress.py

--- plugins/functions/display_progress.py
+++ plugins/functions/display_progress.py
@@ -52,18 +52,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             pass
 
 
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2 ** 10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
 
 
 def TimeFormatter(milliseconds: int) -> str:
