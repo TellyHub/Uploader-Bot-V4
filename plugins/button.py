6c98580a3ac752f720677b1c0e831e91164eb2be
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-03 14:44:36 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -237,7 +237,7 @@ async def youtube_dl_call_back(bot, update):
             
                 
                 else:
-                    logger.info("Did this happen? :\\")
+                    LOGGER.info("Did this happen? :\\")
                 end_two = datetime.now()
                 time_taken_for_upload = (end_two - end_one).seconds
                 try:
