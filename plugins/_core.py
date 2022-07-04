6b2308feb8db2c415745814ff63793572654a129
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-04 09:14:04 +0530

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -16,10 +16,7 @@ import subprocess
 import time
 
 # the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+from plugins.config import config
 
 # the Strings used for this "thing"
 from translation import Translation
@@ -93,25 +90,15 @@ def cult_small_video(video_file, output_directory, start_time, end_time):
     return out_put_file_name
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.text & pyrogram.Filters.private, group=1)
+@Client.on_message(filters.text & pyrogram.Filters.private, group=1)
 def echo(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/echo")
-    bot.send_chat_action(
-        chat_id=update.from_user.id,
-        action="typing"
-    )
+
     text = update.text
     if(text.startswith("http")):
         url = text
         if "|" in url:
-            if str(update.from_user.id) not in Config.SUPER_DLBOT_USERS:
-                bot.send_message(
-                    chat_id=update.from_user.id,
-                    text=Translation.NOT_AUTH_USER_TEXT,
-                    reply_to_message_id=update.message_id
-                )
-                return
+
             thumb_image_path = Config.DOWNLOAD_LOCATION + \
                 "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
@@ -135,7 +122,7 @@ def echo(bot, update):
             a = bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.DOWNLOAD_START,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             after_download_path = DownLoadFile(
                 url, Config.DOWNLOAD_LOCATION + "/" + file_name)
@@ -148,14 +135,14 @@ def echo(bot, update):
             bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.from_user.id,
-                message_id=a.message_id
+                message_id=a.id
             )
             file_size = os.stat(after_download_path).st_size
             if file_size > Config.TG_MAX_FILE_SIZE:
                 bot.edit_message_text(
                     text=Translation.RCHD_TG_API_LIMIT,
                     chat_id=update.from_user.id,
-                    message_id=a.message_id
+                    message_id=a.id
                 )
             else:
                 # try to upload file
@@ -165,7 +152,7 @@ def echo(bot, update):
                     caption=description,
                     # reply_markup=reply_markup,
                     thumb=thumb_image_path,
-                    reply_to_message_id=update.message_id
+                    reply_to_message_id=update.id
                 )
                 bot.edit_message_text(
                     text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
@@ -195,7 +182,7 @@ def echo(bot, update):
                 bot.send_message(
                     chat_id=update.from_user.id,
                     text=exc.output.decode("UTF-8"),
-                    reply_to_message_id=update.message_id
+                    reply_to_message_id=update.id
                 )
             else:
                 # logger.info(t_response)
@@ -215,7 +202,7 @@ def echo(bot, update):
                             "video", format_id, format_ext)
                         if not "audio only" in format_string:
                             ikeyboard = [
-                                pyrogram.InlineKeyboardButton(
+                                InlineKeyboardButton(
                                     "[" + format_string +
                                     "] (" + format_ext + " - " +
                                     approx_file_size + ")",
@@ -225,12 +212,12 @@ def echo(bot, update):
                             inline_keyboard.append(ikeyboard)
                     cb_string = "{}|{}|{}".format("audio", "5", "mp3")
                     inline_keyboard.append([
-                        pyrogram.InlineKeyboardButton(
+                        InlineKeyboardButton(
                             "MP3 " + "(" + "medium" + ")", callback_data=cb_string.encode("UTF-8"))
                     ])
                     cb_string = "{}|{}|{}".format("audio", "0", "mp3")
                     inline_keyboard.append([
-                        pyrogram.InlineKeyboardButton(
+                        InlineKeyboardButton(
                             "MP3 " + "(" + "best" + ")", callback_data=cb_string.encode("UTF-8"))
                     ])
                 else:
@@ -239,7 +226,7 @@ def echo(bot, update):
                     cb_string = "{}|{}|{}".format(
                         "file", format_id, format_ext)
                     inline_keyboard.append([
-                        pyrogram.InlineKeyboardButton(
+                        InlineKeyboardButton(
                             "unknown video format", callback_data=cb_string.encode("UTF-8"))
                     ])
                 reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
@@ -256,7 +243,7 @@ def echo(bot, update):
                     text=Translation.FORMAT_SELECTION.format(thumbnail),
                     reply_markup=reply_markup,
                     parse_mode=pyrogram.ParseMode.HTML,
-                    reply_to_message_id=update.message_id
+                    reply_to_message_id=update.id
                 )
     elif "===" in text:
         logger.info("cult_small_video")
@@ -264,14 +251,14 @@ def echo(bot, update):
             bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.NOT_AUTH_USER_TEXT,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             return
         if update.reply_to_message is not None:
             a = bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.DOWNLOAD_START,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             url = update.reply_to_message.text
             for entity in update.reply_to_message.entities:
@@ -283,7 +270,7 @@ def echo(bot, update):
             bot.edit_message_text(
                 text=Translation.SAVED_RECVD_DOC_FILE,
                 chat_id=update.from_user.id,
-                message_id=a.message_id
+                message_id=a.id
             )
             thumb_image_path = Config.DOWNLOAD_LOCATION + \
                 "/" + str(update.from_user.id) + ".jpg"
@@ -317,7 +304,7 @@ def echo(bot, update):
             bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.from_user.id,
-                message_id=a.message_id
+                message_id=a.id
             )
             bot.send_video(
                 chat_id=update.from_user.id,
@@ -329,20 +316,20 @@ def echo(bot, update):
                 supports_streaming=True,
                 # reply_markup=reply_markup,
                 thumb=thumb_image_path,
-                reply_to_message_id=update.reply_to_message.message_id
+                reply_to_message_id=update.id
             )
             os.remove(mp4_file)
             bot.edit_message_text(
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
                 chat_id=update.from_user.id,
-                message_id=a.message_id,
+                message_id=a.id,
                 disable_web_page_preview=True
             )
         else:
             bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.START_TEXT,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
     elif ":" in text:
         logger.info("take_screen_shot")
@@ -350,14 +337,14 @@ def echo(bot, update):
             bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.FF_MPEG_RO_BOT_RE_SURRECT_ED,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             return
         if update.reply_to_message is not None:
             a = bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.DOWNLOAD_START,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             url = update.reply_to_message.text
             for entity in update.reply_to_message.entities:
@@ -368,7 +355,7 @@ def echo(bot, update):
             bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.from_user.id,
-                message_id=a.message_id
+                message_id=a.id
             )
             bot.send_document(
                 chat_id=update.from_user.id,
@@ -376,7 +363,7 @@ def echo(bot, update):
                 # caption=description,
                 # reply_markup=reply_markup,
                 # thumb=thumb_image_path,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             bot.send_photo(
                 chat_id=update.from_user.id,
@@ -384,7 +371,7 @@ def echo(bot, update):
                 # caption=description,
                 # reply_markup=reply_markup,
                 # thumb=thumb_image_path,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
             os.remove(img_file)
             bot.edit_message_text(
@@ -397,11 +384,11 @@ def echo(bot, update):
             bot.send_message(
                 chat_id=update.from_user.id,
                 text=Translation.FF_MPEG_RO_BOT_RE_SURRECT_ED,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
 
 
-@pyrogram.Client.on_callback_query()
+@Client.on_callback_query()
 def button(bot, update):
     # logger.info(update)
     cb_data = update.data.decode("UTF-8")
@@ -413,7 +400,7 @@ def button(bot, update):
         bot.edit_message_text(
             chat_id=update.from_user.id,
             text=Translation.NOT_AUTH_USER_TEXT,
-            message_id=update.message.message_id
+            message_id=update.id
         )
         return
     thumb_image_path = Config.DOWNLOAD_LOCATION + \
@@ -487,7 +474,7 @@ def button(bot, update):
             bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.from_user.id,
-                message_id=a.message_id
+                message_id=a.id
             )
             try:
                 logger.info(command_to_exec)
@@ -498,7 +485,7 @@ def button(bot, update):
                 bot.edit_message_text(
                     chat_id=update.from_user.id,
                     text=exc.output.decode("UTF-8"),
-                    message_id=a.message_id
+                    message_id=a.id
                 )
             else:
                 t_response_arry = t_response.decode(
@@ -508,7 +495,7 @@ def button(bot, update):
                     text=Translation.AFTER_GET_DL_LINK.format(
                         t_response_arry, max_days),
                     parse_mode=pyrogram.ParseMode.HTML,
-                    message_id=a.message_id,
+                    message_id=a.id,
                     disable_web_page_preview=True
                 )
                 try:
@@ -553,7 +540,7 @@ def button(bot, update):
                     # title=response_json["title"],
                     # reply_markup=reply_markup,
                     thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id
+                    reply_to_message_id=update.id
                 )
             elif tg_send_type == "file":
                 bot.send_document(
@@ -562,7 +549,7 @@ def button(bot, update):
                     thumb=thumb_image_path,
                     caption=description,
                     # reply_markup=reply_markup,
-                    reply_to_message_id=update.message.reply_to_message.message_id
+                    reply_to_message_id=update.id
                 )
             else:
                 bot.send_video(
@@ -575,7 +562,7 @@ def button(bot, update):
                     supports_streaming=True,
                     # reply_markup=reply_markup,
                     thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id
+                    reply_to_message_id=update.id
                 )
             try:
                 os.remove(download_directory)
@@ -585,6 +572,6 @@ def button(bot, update):
             bot.edit_message_text(
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
                 chat_id=update.from_user.id,
-                message_id=update.message.message_id,
+                message_id=update.id,
                 disable_web_page_preview=True
             )
