a660c53a815c9858f48b2ab8ce91d349199c1b15
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-04 08:46:24 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -92,7 +92,7 @@ async def echo(bot, update: Message):
                     "video", format_id, format_ext)
                 if not "audio only" in format_string:
                     ikeyboard = [
-                        pyrogram.types.InlineKeyboardButton(
+                        InlineKeyboardButton(
                             "[" + format_string +
                             "] (" + format_ext + " - " +
                             approx_file_size + ")",
@@ -102,12 +102,12 @@ async def echo(bot, update: Message):
                     inline_keyboard.append(ikeyboard)
             cb_string = "{}|{}|{}".format("audio", "5", "mp3")
             inline_keyboard.append([
-                pyrogram.types.InlineKeyboardButton(
+                InlineKeyboardButton(
                     "MP3 " + "(" + "medium" + ")", callback_data=cb_string.encode("UTF-8"))
             ])
             cb_string = "{}|{}|{}".format("audio", "0", "mp3")
             inline_keyboard.append([
-                pyrogram.types.InlineKeyboardButton(
+                InlineKeyboardButton(
                     "MP3 " + "(" + "best" + ")", callback_data=cb_string.encode("UTF-8"))
             ])
         else:
@@ -116,10 +116,10 @@ async def echo(bot, update: Message):
             cb_string = "{}|{}|{}".format(
                 "file", format_id, format_ext)
             inline_keyboard.append([
-                pyrogram.types.InlineKeyboardButton(
+                InlineKeyboardButton(
                     "unknown video format", callback_data=cb_string.encode("UTF-8"))
             ])
-        reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
+        reply_markup = InlineKeyboardMarkup(inline_keyboard)
         LOGGER.info(reply_markup)
 
         
